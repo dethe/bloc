943705da5adafc1cf00cefa73386d0ad32f10711
Dethe <dethe@livingcode.org>
2025-09-11 22:42:45 -0700

    remove code from github

--- examples/pentagons.js
+++
@@ -1,1 +0,0 @@
-file.examples.pentagons = [["Pen up"],["Right",144],["Forward",50],["Left",144],["Pen down"],["Repeat",5,[["Repeat",10,[["Right",36],["Forward",20]]],["Right",338],["Forward",100],["Left",50]]],["Hide turtle"],["Pen up"],["Right",104],["Forward",-43],["Left",104],["Pen down"],["Repeat",5,[["Repeat",10,[["Right",36],["Forward",10]]],["Right",338],["Forward",50],["Left",50]]]];
